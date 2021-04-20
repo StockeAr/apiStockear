a526d3d2038e911800c2785bff90a407a6ca5aea
Juan Tapia <tapiahuanca.juanluis@gmail.com>
2021-04-20 19:16:17 -0300

    deploy con bd

--- api_stockear/ormconfig.js
+++ api_stockear/ormconfig.js
@@ -1,10 +1,10 @@
-module.exports={
+module.exports = {
    "type": "mysql",
-   "host": "localhost",
+   "host": "@us-cdbr-east-02.cleardb.com",
    "port": 3306,
-   "username": "root",
-   "password": "root",
-   "database": "login_node",
+   "username": "b55f79f71151d2",
+   "password": "0d901c23",
+   "database": "heroku_b8b9f13ef66c1bf	",
    "synchronize": true,
    "logging": false,
    "entities": [
